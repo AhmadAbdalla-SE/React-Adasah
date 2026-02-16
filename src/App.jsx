fc2ba65825c2a6811483787244c4dff31ad1c5f1
AhmadAbdalla-SE <engahmedabdallawork@gmail.com>
2026-02-16 08:20:12 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import About from './components/Pages/About'
 import Blog from './components/Pages/Blog/Blog'
 import Home from './components/Pages/Home/Home'
 import NotFound from './components/Pages/NotFound'
 import Layout from './Layout/Layout'
 
-const routes = createBrowserRouter([
+const routes = createHashRouter([
     {path:'/', element:<Layout/>, children:[
         {index:true, element:<Home/>},
         {path:'/home', element:<Home/>},
